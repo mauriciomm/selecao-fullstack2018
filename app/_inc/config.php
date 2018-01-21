e1d971a0ee8fff78b9ee4c7e9b40a978e9d2f994
Mauricio Marques <mauriciommfsa@gmail.com>
2018-01-20 23:37:05 -0300

    informações de banco

--- app/_inc/config.php
+++ app/_inc/config.php
@@ -9,12 +9,23 @@
 // define('MYSQL_TIMEZONE', '-03:00');
 // define('PHP_TIMEZONE', 'America/Bahia');
 
+// define('SERVER', 'D');
+// define('MYSQL_HOST', 'us-cdbr-iron-east-05.cleardb.net');
+// define('MYSQL_PORT', '3306');
+// define('MYSQL_USER', 'b7d2cc9f411d8b');
+// define('MYSQL_PASS', '52842e56');
+// define('MYSQL_BASE', 'heroku_2142a3e2028a1d6');
+// define('MYSQL_CHARSET', 'utf8');
+// define('MYSQL_TIMEZONE', '-03:00');
+// define('PHP_TIMEZONE', 'America/Bahia');
+
+
 define('SERVER', 'D');
-define('MYSQL_HOST', 'us-cdbr-iron-east-05.cleardb.net');
+define('MYSQL_HOST', 'cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 define('MYSQL_PORT', '3306');
-define('MYSQL_USER', 'b7d2cc9f411d8b');
-define('MYSQL_PASS', '52842e56');
-define('MYSQL_BASE', 'heroku_2142a3e2028a1d6');
+define('MYSQL_USER', 'ou1amd3du8xzowsb');
+define('MYSQL_PASS', 's3szx0nymmwlrm89');
+define('MYSQL_BASE', 'u1gly9alojbmyuy3');
 define('MYSQL_CHARSET', 'utf8');
 define('MYSQL_TIMEZONE', '-03:00');
 define('PHP_TIMEZONE', 'America/Bahia');
