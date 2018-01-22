371d74809aab46b41d697604366470251ad72cca
Mauricio Marques <mauriciommfsa@gmail.com>
2018-01-22 17:11:18 -0300

    Configuração local do banco

--- app/_inc/config.php
+++ app/_inc/config.php
@@ -1,20 +1,20 @@
 <?php
-// define('SERVER', 'D');
-// define('MYSQL_HOST', 'localhost');
-// define('MYSQL_PORT', '3306');
-// define('MYSQL_USER', 'selecao_fullstack');
-// define('MYSQL_PASS', 'barcelona');
-// define('MYSQL_BASE', 'selecao_fullstack');
-// define('MYSQL_CHARSET', 'utf8');
-// define('MYSQL_TIMEZONE', '-03:00');
-// define('PHP_TIMEZONE', 'America/Bahia');
-
 define('SERVER', 'D');
-define('MYSQL_HOST', 'us-cdbr-iron-east-05.cleardb.net');
+define('MYSQL_HOST', 'localhost');
 define('MYSQL_PORT', '3306');
-define('MYSQL_USER', 'b7d2cc9f411d8b');
-define('MYSQL_PASS', '52842e56');
-define('MYSQL_BASE', 'heroku_2142a3e2028a1d6');
+define('MYSQL_USER', 'selecao_fullstack');
+define('MYSQL_PASS', 'barcelona');
+define('MYSQL_BASE', 'selecao_fullstack');
 define('MYSQL_CHARSET', 'utf8');
 define('MYSQL_TIMEZONE', '-03:00');
 define('PHP_TIMEZONE', 'America/Bahia');
+
+// define('SERVER', 'D');
+// define('MYSQL_HOST', 'us-cdbr-iron-east-05.cleardb.net');
+// define('MYSQL_PORT', '3306');
+// define('MYSQL_USER', 'b7d2cc9f411d8b');
+// define('MYSQL_PASS', '52842e56');
+// define('MYSQL_BASE', 'heroku_2142a3e2028a1d6');
+// define('MYSQL_CHARSET', 'utf8');
+// define('MYSQL_TIMEZONE', '-03:00');
+// define('PHP_TIMEZONE', 'America/Bahia');
